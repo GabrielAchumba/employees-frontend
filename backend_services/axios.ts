cc0a0812299454e9aa8cb99e8424a3dea871f9c8
GabrielAchumba <achumba.gabriel@yahoo.com>
2023-11-07 12:57:14 +0100

    changed to development

--- backend_services/axios.ts
+++ backend_services/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios' 
-const envVariable = "production"
+const envVariable = "development"
 const $http  = axios.create({ 
     baseURL: envVariable === "production" ? 'https://employeemgt-f61186568148.herokuapp.com/' : 'http://localhost:4000/', //'https://jsonplaceholder.typicode.com/',
     headers: {'Content-Type': 'application/json'},
